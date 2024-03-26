cf9e58c6b4301bc16a6f0cf508ad74e4236d2521
jihyeon <kth083030@naver.com>
2024-03-27 05:46:31 +0900

    에러핸들링추가

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
   const [weather, setWeather] = useState(null);
   const [city, setCity] = useState('');
   const [loading, setLoading] = useState(false);
+  const [apiError, setAPIError] = useState('');
   const cities = ['sweden', 'new york', 'tokyo', 'seattle', 'seoul'];
   // 현재 위치 구하기
   const getCurrentLocation = () => {
@@ -33,13 +34,18 @@ function App() {
 
   // 도시별 날씨 구하기
   const getWeatherByCity = async () => {
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    setLoading(true);
-    let response = await fetch(url);
-    let data = await response.json();
-    // console.log('도시별 날씨 data', data);
-    setWeather(data);
-    setLoading(false);
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+      setLoading(true);
+      let response = await fetch(url);
+      let data = await response.json();
+      // console.log('도시별 날씨 data', data);
+      setWeather(data);
+      setLoading(false);
+    } catch (err) {
+      setAPIError(err.message);
+      setLoading(false);
+    }
   };
 
   /*
@@ -51,30 +57,36 @@ function App() {
 
   // 현재 위치 날씨 구하기
   const getWeatherByCurrentLocation = async (lat, lon) => {
-    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
-    setLoading(true);
-    let response = await fetch(url);
-    let data = await response.json();
-    //console.log('날씨 데이터', data);
-    setWeather(data);
-    setLoading(false);
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+      setLoading(true);
+      let response = await fetch(url);
+      let data = await response.json();
+      //console.log('날씨 데이터', data);
+      setWeather(data);
+      setLoading(false);
 
-    // 일출과 일몰 시간 가져오기
-    const sunrise = new Date(data.sys.sunrise * 1000);
-    const sunset = new Date(data.sys.sunset * 1000);
+      // 일출과 일몰 시간 가져오기
+      const sunrise = new Date(data.sys.sunrise * 1000);
+      const sunset = new Date(data.sys.sunset * 1000);
 
-    // 현재 시간 가져오기
-    const currentTime = new Date();
+      // 현재 시간 가져오기
+      const currentTime = new Date();
 
-    // 현재 시간이 일출과 일몰 사이에 있는지 확인하여 배경 이미지 설정
-    if (currentTime > sunrise && currentTime < sunset) {
-      setBackgroundImage(
-        'https://i.pinimg.com/564x/5f/ae/ff/5faeffd0d801766699bd4ea4c8f1fd92.jpg',
-      );
-    } else {
-      setBackgroundImage(
-        'https://i.pinimg.com/564x/86/20/ee/8620ee1d43949ac559a1f33a3cbd2dfb.jpg',
-      );
+      // 현재 시간이 일출과 일몰 사이에 있는지 확인하여 배경 이미지 설정
+      if (currentTime > sunrise && currentTime < sunset) {
+        setBackgroundImage(
+          'https://i.pinimg.com/564x/5f/ae/ff/5faeffd0d801766699bd4ea4c8f1fd92.jpg',
+        );
+      } else {
+        setBackgroundImage(
+          'https://i.pinimg.com/564x/86/20/ee/8620ee1d43949ac559a1f33a3cbd2dfb.jpg',
+        );
+      }
+    } catch (err) {
+      console.log(err);
+      setAPIError(err.message);
+      setLoading(false);
     }
   };
 
@@ -91,11 +103,15 @@ function App() {
         <div className="container">
           <ClipLoader color="white" loading={loading} size={50} />
         </div>
-      ) : (
+      ) : !apiError ? (
         <div className="weather-area">
           <ClipLoader color="white" loading={loading} size={50} />
           <WeatherBox weather={weather} /> {/*함수도 props로 보내줄 수 있음!*/}
           <WeatherBtn cities={cities} setCity={setCity} selectedCity={city} />
+        </div>
+      ) : (
+        <div className="container">
+          <h2>{apiError}</h2>
         </div>
       )}
     </div>
